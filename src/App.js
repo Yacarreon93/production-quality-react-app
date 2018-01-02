3d6569b5bcb4283cb9524c91e09fd9425ba8b54a
Yasser Carreón <yacasia@gmail.com>
2018-01-02 16:56:19 -0600

    Create todos list

--- src/App.js
+++ src/App.js
@@ -3,16 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [
+        { id: 1, name: 'Learn JSX', isComplete: false },
+        { id: 2, name: 'Build an app', isComplete: false },
+        { id: 3, name: 'Ship it', isComplete: true },
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React Todos</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Todo-app">
+          <form>
+            <input type="text"/>
+          </form>
+          <div className="Todo-list">
+            <ul>
+              {this.state.todos.map((todo) => 
+                <li key={todo.id}>
+                  <input type="checkbox" defaultChecked={todo.isComplete}/> {todo.name}
+                </li>
+              )}              
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
