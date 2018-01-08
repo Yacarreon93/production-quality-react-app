444dc55bffebedc15ca60ac1a7b1dbc31d4bbc62
Yasser Carreon <yasser.carreon@unosquare.com>
2018-01-08 17:40:17 -0600

    Create TodoList component

---
+++ src/components/todo/TodoList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const TodoList = (props) => {
+  return (
+    <div className="Todo-list">
+      <ul>
+        {props.todos.map((todo) =>
+          <li key={todo.id}>
+            <input type="checkbox" defaultChecked={todo.isComplete}/> {todo.name}
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+}
