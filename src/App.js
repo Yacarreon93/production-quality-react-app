55fb18c947d327c9ecdb4777e9415b1bb7c88472
Yasser Cárreon <yacasia@gmail.com>
2018-01-09 15:45:29 -0600

    Check a todo and persist it in state

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { TodoForm, TodoList } from './components/todo'
-import { addTodo, generateId} from './lib/todoHelpers';
+import { addTodo, generateId, findById, toggleTodo, updateTodo } from './lib/todoHelpers';
 
 class App extends Component {
 
@@ -13,6 +13,13 @@ class App extends Component {
       { id: 3, name: 'Ship it', isComplete: true },
     ],
     currentTodo: ''
+  }
+
+  handleToggle = (id) => {
+    const todo = findById(id, this.state.todos)
+    const toggled = toggleTodo(todo)
+    const updatedTodos = updateTodo(this.state.todos, toggled)
+    this.setState({ todos: updatedTodos })
   }
 
   handleSubmit = (evt) => {
@@ -55,6 +62,7 @@ class App extends Component {
             handleSubmit={submitHandler}
             currentTodo={this.state.currentTodo} />
           <TodoList
+            handleToggle={this.handleToggle}
             todos={this.state.todos} />
         </div>
       </div>
--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -4,12 +4,15 @@ import { PropTypes } from 'prop-types'
 export const TodoItem = (props) => {
   return (
     <li>
-      <input type="checkbox" defaultChecked={props.isComplete}/> {props.name}
+      <input type="checkbox"
+        onChange={() => props.handleToggle(props.id)}
+        checked={props.isComplete}/> {props.name}
     </li>
   )
 }
 
 TodoItem.propTypes = {
   name: PropTypes.string.isRequired,
-  isComplete: PropTypes.bool.isRequired
+  isComplete: PropTypes.bool.isRequired,
+  handleToggle: PropTypes.func.isRequired
 }
