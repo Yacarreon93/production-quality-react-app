fb1f421b6add38e6c87b88ccb7d60df326c3d6d3
Yasser Cárreon <yacasia@gmail.com>
2018-01-09 15:02:23 -0600

    Refactor App class using ES6

--- src/App.js
+++ src/App.js
@@ -5,22 +5,17 @@ import { TodoForm, TodoList } from './components/todo'
 import { addTodo, generateId} from './lib/todoHelpers';
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      todos: [
-        { id: 1, name: 'Learn JSX', isComplete: false },
-        { id: 2, name: 'Build an app', isComplete: false },
-        { id: 3, name: 'Ship it', isComplete: true },
-      ],
-      currentTodo: ''
-    }
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleEmptySubmit = this.handleEmptySubmit.bind(this)
+
+  state = {
+    todos: [
+      { id: 1, name: 'Learn JSX', isComplete: false },
+      { id: 2, name: 'Build an app', isComplete: false },
+      { id: 3, name: 'Ship it', isComplete: true },
+    ],
+    currentTodo: ''
   }
 
-  handleSubmit(evt)  {
+  handleSubmit = (evt) => {
     evt.preventDefault()
     const newId = generateId()
     const newTodo = { id: newId, name: this.state.currentTodo, isComplete: false }
@@ -32,14 +27,14 @@ class App extends Component {
     })
   }
 
-  handleEmptySubmit(evt) {
+  handleEmptySubmit = (evt) => {
     evt.preventDefault()
     this.setState({
       errorMessage: 'Please supply a todo name'
     })
   }
 
-  handleInputChange(evt) {
+  handleInputChange = (evt) => {
     this.setState({
       currentTodo: evt.target.value
     })
