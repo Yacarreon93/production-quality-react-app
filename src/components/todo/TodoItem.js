660b36e280c4095cf82545ab6ca3fb3c83476b9f
Yasser Cárreon <yacasia@gmail.com>
2018-01-09 16:55:39 -0600

    Fix partial implementation in TodoItem

--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -3,7 +3,7 @@ import { PropTypes } from 'prop-types'
 import { partial } from '../../lib/utils'
 
 export const TodoItem = (props) => {
-  const handleToggle = () => partial(props.handleToggle, props.id)
+  const handleToggle = partial(props.handleToggle, props.id)
   return (
     <li>
       <input type="checkbox"
